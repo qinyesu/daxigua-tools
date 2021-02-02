9ecc1258546e7917e70d3f6f46e97faf8e81fc46
qinyesu <qinyesu>
2021-02-02 03:49:37 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# daxigua-tools
+
+> 帮助你快速修改合成大西瓜的图片
+
+在线使用：
+
+[魔改大西瓜项目](https://github.com/liyupi/daxigua)
+
+### 使用方式
+
+**千万不要刷新页面，否则要全部重新上传！**
+
+1. 在对应位置上传你要替换的图片，尺寸必须一致
+
+2. 点击打包下载
+
+3. 解压下载的压缩包，替换掉 res/raw-assets 目录
+
+
+### 本地启动
+
+1. 安装依赖：
+
+    ```bash
+    npm i
+    ```
+
+2. 进入 daxigua-tools 目录，本地运行，支持热更新：
+
+    ```bash
+    npm run serve
+    ```
+   
+3. 打开浏览器访问 localhost:8080 即可！
+
+
+### 一键部署
+
+点击下方按钮，无需任何操作，即可自己部署工具到腾讯云上 ⬇️
+
+[![](https://main.qcloudimg.com/raw/67f5a389f1ac6f3b4d04c7256438e44f.svg)](https://console.cloud.tencent.com/tcb/env/index?action=CreateAndDeployCloudBaseProject&appUrl=https://github.com/liyupi/daxigua-tools&branch=master)
+
+
+
---
+++ src/App.vue
@@ -0,0 +1,98 @@
+<template>
+  <div id="app">
+    <h1>🍉 合成大西瓜改图工具 1.0 - by 鱼皮</h1>
+    <p class="text">
+      请使用电脑打开，用法：<br />
+      <b>千万不要刷新页面！否则要重新上传！</b> <br />
+      1. 在对应位置上传你要替换的图片，尺寸必须一致 <br />
+      2. 点击打包下载 <br />
+      3. 解压下载的压缩包，替换掉 res/raw-assets 目录 <br />
+      <a href="https://github.com/liyupi/daxigua" target="_blank" class="link">项目仓库</a>
+      <a href="https://mp.weixin.qq.com/s/H9VR1MWn-9bKSC_1l_MkJw" target="_blank" class="link">文章教程</a>
+      <a href="https://www.bilibili.com/video/BV1Vy4y1n7KW/" target="_blank" class="link">视频教程</a> <br />
+      作者鱼皮，鹅厂小开发，<a href="https://github.com/liyupi/daxigua" target="_blank">编程技术公众号：程序员鱼皮</a>，欢迎关注 ❤️<br />
+    </p>
+    <el-row :gutter="20" justify="center">
+      <el-col :span="6" v-for="(item, i) in UPLOAD_LIST" :key="i">
+        <div class="grid-content bg-purple item-wrapper">
+          <div class="my-name">{{item.name}} {{item.width}} * {{item.height}}</div>
+          <PicUploader :width="item.width" :height="item.height"
+                       :onChange="file => doPicChange(item, file)" />
+        </div>
+      </el-col>
+    </el-row>
+    <br />
+    <el-button type="primary" @click="doDownload">打包下载</el-button>
+  </div>
+</template>
+
+<script>
+  import PicUploader from "./components/PicUploader";
+  import {DownloadUtil} from "./utils/fileUtils";
+  import {UPLOAD_LIST} from "./constant";
+  import {ZIP_PARENT_DIR} from "./constant";
+
+  export default {
+    name: 'app',
+    components: {PicUploader},
+    data() {
+      return {
+        imageUrl: '',
+        UPLOAD_LIST,
+        ZIP_PARENT_DIR,
+        files: {}
+      }
+    },
+    methods: {
+      // 打包下载
+      doDownload() {
+        const downloadUtil = new DownloadUtil();
+        Object.keys(this.files).forEach(key => {
+          const {file, filename} = this.files[key];
+          if (file && filename) {
+            downloadUtil.addFileInZip(filename, file);
+          }
+        })
+        downloadUtil.packageZipAndDownload('raw-assets.zip');
+      },
+      doPicChange(item, file) {
+        this.files[item.key] = {
+          filename: `${ZIP_PARENT_DIR}/${item.fileDir}/${item.filename}`,
+          file,
+        }
+        console.log(this.files)
+      }
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    padding: 25px;
+  }
+
+  .text {
+    text-align: left;
+    margin: 0 auto;
+    margin-bottom: 50px;
+    line-height: 1.8;
+    display: inline-block;
+  }
+
+  .link {
+    margin-right: 20px;
+  }
+
+  .item-wrapper {
+    margin-bottom: 16px;
+  }
+
+  .my-name {
+    margin-bottom: 16px;
+  }
+</style>
